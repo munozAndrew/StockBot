4162b4b304fb9649545804eeb3e411876b006be0
Andrew <arvizumunozandrew@gmail.com>
2024-08-02 14:05:42 -0700

    Added API keys from alpaca

---
+++ tradingbot.py
@@ -0,0 +1,18 @@
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
+
+from config import API_SECRET, API_KEY
+
+#API_KEY = ""
+#API_SECRET = ""
+BASE_URL = ""
+
+ALPACA_CREDS = {
+    
+    "API_KEY":API_KEY,
+    "API_SECRET":API_SECRET,
+    "PAPER": True
+}
