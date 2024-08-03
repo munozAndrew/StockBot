b1cb7968afcbd1a1b9cae7cde4d0aabc6e2c0e24
Andrew <arvizumunozandrew@gmail.com>
2024-08-03 15:57:39 -0700

    API test run woring, adding logic to change number of items bought

--- tradingbot.py
+++ tradingbot.py
@@ -8,7 +8,7 @@ from config import API_SECRET, API_KEY
 
 #API_KEY = ""
 #API_SECRET = ""
-BASE_URL = "https://api.alpaca.markets"
+BASE_URL = "https://paper-api.alpaca.markets/v2"
 
 ALPACA_CREDS = {
     
@@ -27,22 +27,31 @@ class MLTrader(Strategy):
         
     #runs after everytick (new data from data source is recieved)
     def on_trading_iteration(self):
+        
+        if self.last_trade == None:
+            order = self.create_order(
+                self.symbol,
+                10,
+                "buy",
+                type="market"
+            )
+            self.submit_order(order)
+            self.last_trade = "buy"
         return super().on_trading_iteration()
 
 
-start_date = datetime(2023, 11, 20)
-end_date = datetime(2023, 12, 3)
+start_date = datetime(2023, 12, 10)
+end_date = datetime(2023, 12, 15)
 
 #creates instancce of alpaca, interacts with API for trading - 
 # placing orders, viewing market data, checking account details
 broker = Alpaca(ALPACA_CREDS)
 
-Strategy = MLTrader(name='mlstrat', 
-                    broker=broker, 
+strategy = MLTrader(name='mlstrat', broker=broker, 
                     parameters={})
 
 #how well it runs based on historic data
-Strategy.backtest(
+strategy.backtest(
     YahooDataBacktesting,
     start_date,
     end_date,
