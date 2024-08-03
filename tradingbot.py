b9a00462fda0e2a2b6b2d1c6ec7e8b86cfc8596c
Andrew <arvizumunozandrew@gmail.com>
2024-08-03 16:23:52 -0700

    created dynamic quantity sizing

--- tradingbot.py
+++ tradingbot.py
@@ -20,23 +20,35 @@ ALPACA_CREDS = {
 
 class MLTrader(Strategy):
     #runs once when bot is started up
-    def initialize(self, symbol:str="SPY"):
+    def initialize(self, symbol:str="SPY", cash_at_risk:float=.5):
         self.symbol = symbol
         self.sleeptime = "24H"
         self.last_trade = None
+        self.cash_at_risk = cash_at_risk
         
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price,0)
+        return cash, last_price, quantity
+        
+    
     #runs after everytick (new data from data source is recieved)
     def on_trading_iteration(self):
         
-        if self.last_trade == None:
-            order = self.create_order(
-                self.symbol,
-                10,
-                "buy",
-                type="market"
-            )
-            self.submit_order(order)
-            self.last_trade = "buy"
+        cash, last_price, quantity = self.position_sizing()
+        
+        if cash > last_price:
+            
+            if self.last_trade == None:
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type="market"
+                )
+                self.submit_order(order)
+                self.last_trade = "buy"
         return super().on_trading_iteration()
 
 
@@ -48,12 +60,12 @@ end_date = datetime(2023, 12, 15)
 broker = Alpaca(ALPACA_CREDS)
 
 strategy = MLTrader(name='mlstrat', broker=broker, 
-                    parameters={})
+                    parameters={"symbol":"SPY", "cash_at_risk":.5})
 
 #how well it runs based on historic data
 strategy.backtest(
     YahooDataBacktesting,
     start_date,
     end_date,
-    parameters={}
+    parameters={"symbol":"SPY", "cash_at_risk":.5}
 )
